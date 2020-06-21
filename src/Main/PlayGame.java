e317ffcc8e8a20160d172f22660cbdb4b990ec54
Hung <mhungict99@gmail.com>
2020-06-22 00:31:10 +0700

    99 percent

--- src/Main/PlayGame.java
+++ src/Main/PlayGame.java
@@ -24,6 +24,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import javafx.scene.shape.Circle;
+
 public class PlayGame implements Runnable {
 
 	private String ip = "localhost";
@@ -44,28 +46,40 @@ public class PlayGame implements Runnable {
 	private BufferedImage galaxy;
 	private BufferedImage dan;
 	private BufferedImage maybay;
-	private BufferedImage tenlua;
+	private BufferedImage tenlua2;
+	private BufferedImage tenlua1;
 	ArrayList<BufferedImage> danArray = new ArrayList<BufferedImage>();
 
-	private boolean circle = true;
+	private boolean client_join = true;
 	private boolean accepted = false;
-	private int x_tenlua = 350;
-	private int y_tenlua = 490;
-	private int size_tenlua_x = 70;
-	private int size_tenlua_y = 70;
 
-	private int x_dan = x_tenlua;
-	private int y_dan = y_tenlua;
+	private int x_tenlua_1 = 350;
+	private int y_tenlua_1 = 490;
+	private int size_tenlua_x_1 = 70;
+	private int size_tenlua_y_1 = 70;
+
+	private int x_tenlua_2 = 350;
+	private int y_tenlua_2 = 500;
+	private int size_tenlua_x_2 = 70;
+	private int size_tenlua_y_2 = 70;
+
+	private int x_dan = x_tenlua_1;
+	private int y_dan = y_tenlua_1;
 	private int size_dan_x = 40;
 	private int size_dan_y = 40;
 
 	private boolean[][] listdan = new boolean[10][16];
 	private boolean[][] listmb = new boolean[11][16];
+	private boolean status_tl_1 = true;// TRANG THAI TEN LUA 1( SONG / CHET)
+	private boolean status_tl_2 = true;// TRANG THAI TEN LUA 2( SONG / CHET)
+
 	private Timer timer_dan;
 	private Timer timer_mb;
+	private Timer timer_dis;
 	public int time = 50;
-	public int time_mb = 200;
-	
+	public int time_appear = 400;
+	public int time_flight = 400;
+
 	Random generator = new Random();
 
 	/**
@@ -87,7 +101,7 @@ public class PlayGame implements Runnable {
 			System.out.println("The port you entered was invalid, please input another port: ");
 			port = scanner.nextInt();
 		}
-	
+
 		for (int i = 0; i < 10; i++) {
 			for (int j = 1; j < 16; j++) {
 				listmb[i][j] = false;
@@ -98,12 +112,12 @@ public class PlayGame implements Runnable {
 			for (int j = 0; j < 16; j++) {
 				listdan[i][j] = false;
 			}
-		};
+		}
+		;
 		loadImages();
-		
+
 		painter = new Painter();
 		painter.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		
 
 		if (!connect()) {
 			initializeServer();
@@ -123,13 +137,13 @@ public class PlayGame implements Runnable {
 		frame.setVisible(true);
 
 		thread = new Thread(this, "PlayGame");
+		thread.setDaemon(true);
 		thread.start();
 	}
 
 	public void run() {
 		while (true) {
 			tick();
-			painter.repaint();
 			if (!accepted) {
 				listenForServerRequest();
 				painter.repaint();
@@ -137,46 +151,61 @@ public class PlayGame implements Runnable {
 
 		}
 	}
-	
+
 	private void tick() {
-		try {
-			int input = dis.readInt();
-			if (input % 50 == 0) {
-				x_tenlua = input;
+		if (status_tl_1 || status_tl_2) {
+			try {
+				int input = dis.readInt();
+				if (input % 50 == 0) {
+					if (!client_join)
+						x_tenlua_1 = input;
+					else
+						x_tenlua_2 = input;
+
+				}
+				if (input % 17 == 0) {
+					listmb[0][input / 17] = true;
+				} else {
+					listdan[9][input] = true;
+				}
+
+				painter.repaint();
+			} catch (Exception e) {
+				// TODO: handle exception
 			}
-			else {
-				listdan[9][input] = true;
-				//listmb[0][input] = true;
-			}
-			String mb = dis.readLine();
-			painter.repaint();
-		} catch (Exception e) {
-			// TODO: handle exception
+		} else{
+			accepted = false;
+			thread.stop();
 		}
 
 	}
-	
+
 	private void render(Graphics g) {// KHI THUC HIEN REPAINT => VE LAI DO HOA
 
-		//g.drawImage(galaxy, 0, 0, null);
+		g.drawImage(galaxy, 0, 0, null);
 		if (accepted) {
-			g.drawImage(tenlua, x_tenlua, y_tenlua, size_tenlua_x, size_tenlua_y, null, null);
+			if (status_tl_1)
+				g.drawImage(tenlua1, x_tenlua_1, y_tenlua_1, size_tenlua_x_1, size_tenlua_y_1, null, null);
+			if (status_tl_2)
+				g.drawImage(tenlua2, x_tenlua_2, y_tenlua_2, size_tenlua_x_2, size_tenlua_y_2, null, null);
 			for (int i = 0; i < 10; i++) {
 				for (int j = 0; j < 16; j++) {
 					if (listdan[i][j] == true)
 						g.drawImage(dan, j * 50 + 15, i * 50, size_dan_x, size_dan_y, null, null);
 				}
-			};
-			for (int i = 0; i < 10; i++) {
+			}
+			;
+			for (int i = 0; i < 11; i++) {
 				for (int j = 0; j < 16; j++) {
-					if (listmb[i][j] == true)
-					{
+					if (listmb[i][j] == true) {
 						g.drawImage(maybay, j * 50 + 10, i * 50, size_dan_x + 15, size_dan_y + 15, null, null);
 					}
 				}
-			};
+			}
+			;
 		}
 	}
+
 	private void listenForServerRequest() {
 		Socket socket = null;
 		try {
@@ -210,13 +239,15 @@ public class PlayGame implements Runnable {
 		} catch (Exception e) {
 			System.out.println("initializeServer");
 		}
+		client_join = false;
 	}
 
 	private void loadImages() {
 		try {
 			galaxy = ImageIO.read(getClass().getResourceAsStream("/galaxy.png"));
-			tenlua = ImageIO.read(getClass().getResourceAsStream("/tenlua.png"));
+			tenlua1 = ImageIO.read(getClass().getResourceAsStream("/tenlua1.png"));
 			maybay = ImageIO.read(getClass().getResourceAsStream("/maybay.png"));
+			tenlua2 = ImageIO.read(getClass().getResourceAsStream("/tenlua2.gif"));
 			dan = ImageIO.read(getClass().getResourceAsStream("/dan.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -225,7 +256,7 @@ public class PlayGame implements Runnable {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		PlayGame galaxy_war = new PlayGame();
+		new PlayGame();
 	}
 
 	private class Painter extends JPanel implements KeyListener, ActionListener {
@@ -238,14 +269,14 @@ public class PlayGame implements Runnable {
 			this.setBackground(Color.white);
 			timer_dan = new Timer(time, this);
 			timer_dan.start();
-			
+
 			Maybay appear = new Maybay();
-			timer_mb = new Timer(time_mb, appear);
+			timer_mb = new Timer(time_appear, appear);
 			timer_mb.start();
-			
+
 			Distance dis = new Distance();
-			Timer mm = new Timer(400, dis);
-			mm.start();
+			timer_dis = new Timer(time_flight, dis);
+			timer_dis.start();
 		}
 
 		@Override
@@ -262,45 +293,78 @@ public class PlayGame implements Runnable {
 		public void keyPressed(KeyEvent e) {
 
 			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-				if (x_tenlua < 750) {
-					x_tenlua += 50;
-					try {
-						dos.writeInt(x_tenlua);
-						dos.flush();
-					} catch (Exception e2) {
-						// TODO: handle exception
+				if (client_join) {
+					if (x_tenlua_1 < 750) {
+						x_tenlua_1 += 50;
+						try {
+							dos.writeInt(x_tenlua_1);
+							dos.flush();
+						} catch (Exception e2) {
+							// TODO: handle exception
+						}
 					}
-					repaint();
+				} else {
+					if (x_tenlua_2 < 750) {
+						x_tenlua_2 += 50;
+						try {
+							dos.writeInt(x_tenlua_2);
+							dos.flush();
+						} catch (Exception e2) {
+						}
+					}
 				}
 			}
 
 			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-				if (x_tenlua > 50) {
-					x_tenlua -= 50;
-					try {
-						dos.writeInt(x_tenlua);
-						dos.flush();
-					} catch (Exception e2) {
-						// TODO: handle exception
+				if (client_join) {
+					if (x_tenlua_1 > 50) {
+						x_tenlua_1 -= 50;
+						try {
+							dos.writeInt(x_tenlua_1);
+							dos.flush();
+						} catch (Exception e2) {
+							// TODO: handle exception
+						}
 					}
-					repaint();
+				} else {
+					if (x_tenlua_2 > 50) {
+						x_tenlua_2 -= 50;
+						try {
+							dos.writeInt(x_tenlua_2);
+							dos.flush();
+						} catch (Exception e2) {
+							// TODO: handle exception
+						}
+					}
 				}
 			}
 			if (e.getKeyCode() == KeyEvent.VK_UP) {
-				int dan = x_tenlua / 50;
-				listdan[9][dan] = true;
-				repaint();
+				if (client_join && status_tl_1) {
+					int dan = x_tenlua_1 / 50;
+					listdan[9][dan] = true;
+					repaint();
 
-				try {
-					dos.writeInt(dan);
-					dos.flush();
-				} catch (IOException e1) {
-					e1.printStackTrace();
+					try {
+						dos.writeInt(dan);
+						dos.flush();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				} else if (status_tl_2) {
+					int dan = x_tenlua_2 / 50;
+					listdan[9][dan] = true;
+
+					try {
+						dos.writeInt(dan);
+						dos.flush();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
 				}
 
-				// System.out.println(dan);
 			}
 			Toolkit.getDefaultToolkit().sync();
+			repaint();
 
 		}
 
@@ -310,42 +374,44 @@ public class PlayGame implements Runnable {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			//System.out.println("painter go");
+			// System.out.println("painter go");
 			for (int i = 0; i < 10; i++) {
 				for (int j = 0; j < 16; j++) {
 					if (listdan[i][j] == true) {
 						listdan[i][j] = false;
-						if (listmb[i][j]==true) {
-							listmb[i][j]=false;
-							
-						}else if (i > 0)
+						if (listmb[i][j] == true) {
+							listmb[i][j] = false; // NEU DAN GAP PHAI MAY BAY BAY XUONG THI BIEN MAT CA 2
+						} else if (i > 0)
 							listdan[i - 1][j] = true;
-						
 					}
 				}
 			}
 			painter.repaint();
 		}
-}
+	}
 
-	
-	private class Maybay implements ActionListener {//khoang cach de may bay bay xuong
+	private class Maybay implements ActionListener {// khoang cach de may bay bay xuong
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			timer_mb.start();
-			for (int i = 9; i >= 0; i--) {
-				for (int j = 15; j >= 0; j--) {
-					if (listmb[i][j] == true) {
-						listmb[i][j] = false;
-						if (i >= 0 && i < 9)
-						{
-							listmb[i+1][j] = true;
-							painter.repaint();
-						}
-//						if(i == 10) {
+			if (accepted) {
+				timer_mb.start();
+				for (int i = 10; i >= 0; i--) {
+					for (int j = 15; j >= 0; j--) {
+						if (listmb[i][j] == true) {
+							listmb[i][j] = false;
+							if (i >= 0 && i < 10) {
+								listmb[i + 1][j] = true;
+								painter.repaint();
+								if ((x_tenlua_1 == j * 50) && ((i + 1) == 10))
+									status_tl_1 = false;
+								if ((x_tenlua_2 == j * 50) && ((i + 1) == 10))
+									status_tl_2 = false;
+							}
+//						if(i == 10) {						
 //							listmb[0][j] = true;
 //						}
+						}
 					}
 				}
 			}
@@ -353,22 +419,24 @@ public class PlayGame implements Runnable {
 		}
 
 	}
-	
-	private class Distance implements ActionListener{ //Khoang thoi gian tao ra 2 may bay 
+
+	private class Distance implements ActionListener { // Khoang thoi gian tao ra 2 may bay
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			int temp = generator.nextInt(15) + 1;
-			listmb[0][temp] = true;
-			try {
-				dos.writeChars(String.valueOf(temp));
-				dos.flush();
-			} catch (Exception e2) {
-				// TODO: handle exception
+			if (accepted) {
+//				/System.out.println("START RAN DOM");
+				int temp = generator.nextInt(15) + 1;
+				listmb[0][temp] = true;
+				try {
+					dos.writeInt(temp * 17);
+					dos.flush();
+				} catch (Exception e2) {
+				}
+				painter.repaint();
 			}
-			painter.repaint();
 		}
-		
+
 	}
 }
